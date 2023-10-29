f95ca864a7aab242052e67112f5ec35a37f9f0bd
phamshellie <105142377+phamshellie@users.noreply.github.com>
2023-10-29 14:54:14 -0500

    Create auth_endpoint.py

---
+++ auth_endpoint.py
@@ -0,0 +1,27 @@
+# /auth endpoint
+@app.route('/auth', methods=['POST'])
+def auth():
+    expired = request.args.get('expired')
+
+    conn = sqlite3.connect('totally_not_my_privateKeys.db')
+    cursor = conn.cursor()
+
+    if expired:
+        cursor.execute("SELECT key FROM keys WHERE exp <= ?", (datetime.now().timestamp(),))
+    else:
+        cursor.execute("SELECT key FROM keys WHERE exp > ?", (datetime.now().timestamp(),))
+
+    private_key_pem = cursor.fetchone()[0]
+
+    # authentication
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    if username == 'userABC' and password == 'password123':
+        payload = {'username': username}
+        token = jwt.encode(payload, private_key_pem, algorithm='RS256')
+        conn.close()
+        return jsonify({'token': token.decode('utf-8')})
+    else:
+        conn.close()
+        return jsonify({'message': 'Authentication failed'}), 401
